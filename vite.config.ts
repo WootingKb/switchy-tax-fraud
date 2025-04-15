79520a7bffefcfd49effd3744260dd90d9a8497b
simon-wh <simon@wooting.io>
2025-04-15 10:34:47 +0200

    Update vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // This needs to be the name of the repo to host the game on github pages
+  base: "/gamejam-template/",
   plugins: [react()],
-})
+});
